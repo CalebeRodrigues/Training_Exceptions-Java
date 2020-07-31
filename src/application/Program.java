923ef09a0968f22cdd59055bcd4988b15c5ce6ef
Calebe Rodrigues <calebebrodrigues@gmail.com>
2020-07-31 15:49:14 -0300

    Finished Project

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,9 +1,43 @@
 package application;
 
+import java.util.Locale;
+import java.util.Scanner;
+
+import model.entities.Account;
+import model.exceptions.DomainExceptions;
+
 public class Program {
 
 	public static void main(String[] args) {
 		
+		Locale.setDefault(Locale.US);
+		Scanner scanner = new Scanner(System.in);
+		
+		System.out.println("Enter account data: ");
+		System.out.print("Number: ");
+		int number = scanner.nextInt();
+		System.out.print("Holder: ");
+		scanner.nextLine();
+		String holder = scanner.nextLine();
+		System.out.print("Initial balance: ");
+		Double balance = scanner.nextDouble();
+		System.out.print("Withdraw limit: ");
+		Double withdrawLimit = scanner.nextDouble();
+		
+		Account account = new Account(number, holder, balance, withdrawLimit);
+		System.out.println();
+		
+		try {
+			System.out.print("Enter amount for withdraw: ");
+			Double amount = scanner.nextDouble();
+			account.withDraw(amount);
+			System.out.println(account);
+		}
+		catch (DomainExceptions e) {
+			System.out.print("Withdraw error: " + e.getMessage());
+		}
+		
+		scanner.close();
 	}
 
 }
--- src/model/entities/Account.java
+++ src/model/entities/Account.java
@@ -1,9 +1,64 @@
 package model.entities;
 
+import model.exceptions.DomainExceptions;
+
 public class Account {
+	
 	private Integer number;
 	private String holder;
 	private Double balance;
 	private Double withdrawLimit;
 	
+	public Account() {
+		
+	}
+	
+	public Account(Integer number, String holder, Double balance, Double withdrawLimit) {
+		this.number = number;
+		this.holder = holder;
+		this.balance = balance;
+		this.withdrawLimit = withdrawLimit;
+	}
+
+	public Integer getNumber() {
+		return number;
+	}
+
+	public String getHolder() {
+		return holder;
+	}
+
+		public Double getBalance() {
+		return balance;
+	}
+
+	public Double getWithdrawLimit() {
+		return withdrawLimit;
+	}
+
+	public void setWithdrawLimit(Double withdrawLimit) {
+		this.withdrawLimit = withdrawLimit;
+	}
+	
+	public void deposit(Double amount){
+		balance += amount;
+	}
+	
+	public void withDraw(Double amount) {
+		
+		if(amount > withdrawLimit) {
+			throw new DomainExceptions("The amount exceeds withdraw limit");
+		}
+		
+		if(amount > balance) {
+			throw new DomainExceptions("Not enough balance");
+		}
+			
+		balance -= amount;
+	}
+	
+	public String toString() {
+		
+		return "New balance: $ " + String.format("%.2f",balance); 
+	}
 }
